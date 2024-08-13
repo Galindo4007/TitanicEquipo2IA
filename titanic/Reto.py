955ae7dc846e78f0d84b6a4805917e3ad09cc86e
a01197699 <122946366+a01197699@users.noreply.github.com>
2024-08-12 19:12:38 -0600

    Update Reto.py

--- titanic/Reto.py
+++ titanic/Reto.py
@@ -1,2 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sbn
 
-df_gender = 
+df_gender = pd.read_csv('gender_submission.csv')
+df_train = pd.read_csv('train.csv')
+df_test = pd.read_csv('test.csv')
+
+print(df_gender.head())
